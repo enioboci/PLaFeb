92c7c94fb93fee9155f934a7e8581c9cd0e5ec71
Enio Boci <enioboci2@gmail.com>
2026-03-08 22:56:35 -0400

    Complete HW3

--- src/let/lib/ds.ml
+++ src/let/lib/ds.ml
@@ -2,12 +2,18 @@
 
 (* expressed values and environments are defined mutually recursively *)
 
+type 'a tree =
+  | Empty
+  | Node of 'a * 'a tree * 'a tree
 
 type exp_val =
   | NumVal of int
   | BoolVal of bool
   | PairVal of exp_val*exp_val
   | TupleVal of exp_val list
+  | TreeVal of exp_val tree
+  | RecordVal of (string * (bool * exp_val)) list
+
 type env =
   | EmptyEnv
   | ExtendEnv of string*exp_val*env
@@ -16,16 +22,16 @@ type env =
 (* Environment Abstracted Result *)
 
 type 'a result = Ok of 'a | Error of string
-
 type 'a ea_result = env -> 'a result
-  
+
 let return : 'a -> 'a ea_result =
   fun v ->
   fun _env ->
   Ok v
 
-let error : string -> 'a ea_result = fun s ->
-  fun _env -> 
+let error : string -> 'a ea_result =
+  fun s ->
+  fun _env ->
   Error s
 
 let (>>=) : 'a ea_result -> ('a -> 'b ea_result) -> 'b ea_result =
@@ -46,7 +52,8 @@ let run : 'a ea_result -> 'a result =
   fun c ->
   c EmptyEnv
 
-let lookup : env ea_result = fun env ->
+let lookup : env ea_result =
+  fun env ->
   Ok env
 
 let rec sequence : 'a ea_result list -> 'a list ea_result =
@@ -57,13 +64,16 @@ let rec sequence : 'a ea_result list -> 'a list ea_result =
     h >>= fun ev ->
     sequence t >>= fun evs ->
     return (ev::evs)
-      
+
+
 (* Operations on environments *)
 
-let empty_env : unit -> env ea_result = fun () ->
+let empty_env : unit -> env ea_result =
+  fun () ->
   return EmptyEnv
 
-let extend_env : string -> exp_val -> env ea_result = fun id v env ->
+let extend_env : string -> exp_val -> env ea_result =
+  fun id v env ->
   Ok (ExtendEnv(id,v,env))
 
 let rec extend_env_list_helper =
@@ -72,15 +82,15 @@ let rec extend_env_list_helper =
   | [],[] -> en
   | id::idt,ev::evt ->
     ExtendEnv(id,ev,extend_env_list_helper idt evt en)
-  | _,_ -> failwith
-             "extend_env_list_helper: ids and evs have different sizes"
-  
+  | _,_ -> failwith "extend_env_list_helper: ids and evs have different sizes"
+
 let extend_env_list =
   fun ids evs ->
   fun en ->
   Ok (extend_env_list_helper ids evs en)
-    
-let rec apply_env : string -> exp_val ea_result = fun id env ->
+
+let rec apply_env : string -> exp_val ea_result =
+  fun id env ->
   match env with
   | EmptyEnv -> Error (id^" not found!")
   | ExtendEnv(v,ev,tail) ->
@@ -89,34 +99,70 @@ let rec apply_env : string -> exp_val ea_result = fun id env ->
     else apply_env id tail
 
 
-
 (* operations on expressed values *)
 
-let int_of_numVal : exp_val -> int ea_result =  function
-  |  NumVal n -> return n
+let int_of_numVal : exp_val -> int ea_result = function
+  | NumVal n -> return n
   | _ -> error "Expected a number!"
 
-let bool_of_boolVal : exp_val -> bool ea_result =  function
-  |  BoolVal b -> return b
+let bool_of_boolVal : exp_val -> bool ea_result = function
+  | BoolVal b -> return b
   | _ -> error "Expected a boolean!"
 
-let list_of_tupleVal : exp_val -> (exp_val list)  ea_result =  function
-  |  TupleVal l -> return l
+let list_of_tupleVal : exp_val -> (exp_val list) ea_result = function
+  | TupleVal l -> return l
   | _ -> error "Expected a tuple!"
-           
-let pair_of_pairVal : exp_val -> (exp_val*exp_val) ea_result =  function
-  |  PairVal(ev1,ev2) -> return (ev1,ev2)
+
+let pair_of_pairVal : exp_val -> (exp_val*exp_val) ea_result = function
+  | PairVal(ev1,ev2) -> return (ev1,ev2)
   | _ -> error "Expected a pair!"
-           
-let rec string_of_expval = function
+
+let tree_of_treeVal : exp_val -> (exp_val tree) ea_result = function
+  | TreeVal t -> return t
+  | _ -> error "Expected a tree!"
+
+let record_of_recordVal : exp_val -> ((string * (bool * exp_val)) list) ea_result = function
+  | RecordVal r -> return r
+  | _ -> error "Expected a record"
+
+let rec has_duplicate_fields : (string * 'a) list -> bool =
+  fun fs ->
+  match fs with
+  | [] -> false
+  | (id,_)::rest ->
+    List.exists (fun (id',_) -> id = id') rest || has_duplicate_fields rest
+
+let rec lookup_record_field : string -> (string * (bool * exp_val)) list -> exp_val ea_result =
+  fun id fs ->
+  match fs with
+  | [] -> error "Proj: field does not exist"
+  | (name,(_,v))::rest ->
+    if name = id
+    then return v
+    else lookup_record_field id rest
+
+let rec string_of_tree t =
+  match t with
+  | Empty -> "Empty"
+  | Node(v,l,r) ->
+    "Node(" ^ string_of_expval v ^ "," ^ string_of_tree l ^ "," ^ string_of_tree r ^ ")"
+
+and string_of_record_fields fs =
+  String.concat "; "
+    (List.map
+       (fun (id,(_,v)) -> "(" ^ id ^ ",(" ^ "false" ^ "," ^ string_of_expval v ^ "))")
+       fs)
+
+and string_of_expval = function
   | NumVal n -> "NumVal " ^ string_of_int n
   | BoolVal b -> "BoolVal " ^ string_of_bool b
-  | PairVal (ev1,ev2) -> "PairVal("^string_of_expval ev1
-                         ^","^ string_of_expval ev2^")"
+  | PairVal (ev1,ev2) -> "PairVal("^string_of_expval ev1 ^","^ string_of_expval ev2^")"
   | TupleVal evs -> "TupleVal("^String.concat "," (List.map string_of_expval evs)^")"
+  | TreeVal t -> "TreeVal (" ^ string_of_tree t ^ ")"
+  | RecordVal fs -> "RecordVal [" ^ string_of_record_fields fs ^ "]"
 
 let rec string_of_env' ac = function
-  | EmptyEnv ->  "["^String.concat ",\n" ac^"]"
+  | EmptyEnv -> "["^String.concat ",\n" ac^"]"
   | ExtendEnv(id,v,env) -> string_of_env' ((id^":="^string_of_expval v)::ac) env
 
 let string_of_env : string ea_result =
--- src/let/lib/interp.ml
+++ src/let/lib/interp.ml
@@ -1,3 +1,6 @@
+(* Name: Enio Boci
+*)
+
 open Parser_plaf.Ast
 open Parser_plaf.Parser
 open Ds
@@ -8,26 +11,31 @@ let rec eval_expr : expr -> exp_val ea_result =
   match e with
   | Int(n) ->
     return (NumVal n)
+
   | Var(id) ->
     apply_env id
+
   | Add(e1,e2) ->
     eval_expr e1 >>=
     int_of_numVal >>= fun n1 ->
     eval_expr e2 >>=
     int_of_numVal >>= fun n2 ->
     return (NumVal (n1+n2))
+
   | Sub(e1,e2) ->
     eval_expr e1 >>=
     int_of_numVal >>= fun n1 ->
     eval_expr e2 >>=
     int_of_numVal >>= fun n2 ->
     return (NumVal (n1-n2))
+
   | Mul(e1,e2) ->
     eval_expr e1 >>=
     int_of_numVal >>= fun n1 ->
     eval_expr e2 >>=
     int_of_numVal >>= fun n2 ->
     return (NumVal (n1*n2))
+
   | Div(e1,e2) ->
     eval_expr e1 >>=
     int_of_numVal >>= fun n1 ->
@@ -36,25 +44,78 @@ let rec eval_expr : expr -> exp_val ea_result =
     if n2==0
     then error "Division by zero"
     else return (NumVal (n1/n2))
+
   | Let(id,def,body) ->
-    eval_expr def >>= 
+    eval_expr def >>=
     extend_env id >>+
     eval_expr body 
+
   | ITE(e1,e2,e3) ->
     eval_expr e1 >>=
     bool_of_boolVal >>= fun b ->
     if b 
     then eval_expr e2
     else eval_expr e3
+
   | IsZero(e) ->
     eval_expr e >>=
     int_of_numVal >>= fun n ->
     return (BoolVal (n = 0))
+
   | Debug(_e) ->
     string_of_env >>= fun str ->
     print_endline str; 
     error "Debug called"
-  | _ -> failwith "Not implemented yet!"
+
+  | IsEmpty(e1) ->
+    eval_expr e1 >>=
+    tree_of_treeVal >>= fun t ->
+    (match t with
+     | Empty -> return (BoolVal true)
+     | Node(_,_,_) -> return (BoolVal false))
+
+  | EmptyTree(_t) ->
+    return (TreeVal Empty)
+
+  | Node(e1,e2,e3) ->
+    eval_expr e1 >>= fun v1 ->
+    eval_expr e2 >>=
+    tree_of_treeVal >>= fun t2 ->
+    eval_expr e3 >>=
+    tree_of_treeVal >>= fun t3 ->
+    return (TreeVal (Node(v1,t2,t3)))
+
+  | CaseT(e1,e2,id1,id2,id3,e3) ->
+    eval_expr e1 >>=
+    tree_of_treeVal >>= fun t ->
+    (match t with
+     | Empty ->
+       eval_expr e2
+     | Node(v,l,r) ->
+       extend_env id1 v >>+
+       extend_env id2 (TreeVal l) >>+
+       extend_env id3 (TreeVal r) >>+
+       eval_expr e3)
+
+  | Record(fs) ->
+    if has_duplicate_fields fs
+    then error "Record: duplicate fields"
+    else
+      let rec eval_fields fs =
+        match fs with
+        | [] -> return []
+        | (id,(b,exp))::rest ->
+          eval_expr exp >>= fun v ->
+          eval_fields rest >>= fun rest_vals ->
+          return ((id,(b,v))::rest_vals)
+      in
+      eval_fields fs >>= fun vals ->
+      return (RecordVal vals)
+
+  | Proj(e1,id) ->
+    eval_expr e1 >>=
+    record_of_recordVal >>= fun fs ->
+    lookup_record_field id fs
 
 (** [eval_prog e] evaluates program [e] *)
 let eval_prog (AProg(_,e)) =
@@ -64,6 +125,3 @@ let eval_prog (AProg(_,e)) =
 let interp (e:string) : exp_val result =
   let c = e |> parse |> eval_prog
   in run c
-  
-
-
